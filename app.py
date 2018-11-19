733171fe3211fac8ce973336d61448a96c3e8d82
igloosi <jani.kraner@gmail.com>
2018-11-19 08:59:15 +0000

    Load sttaic assets directly from the fovuk-frontend package

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, send_from_directory
 app = Flask(__name__)
+
 
 @app.route('/')
 def main():
     return render_template('index.html')
-
+    
+# load assets directly from govuk-frontend package
+@app.route('/assets/<path:filename>')  
+def send_file(filename):  
+    return send_from_directory('node_modules/govuk-frontend/assets/', filename)
 
 if __name__ == '__main__':
     app.run()
